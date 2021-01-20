91e76b9c5610dbfe2f3e3dd203fa2d873a651627
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-01-19 19:04:34 -0800

    cont writing desolve version of network model

--- helpers/r_helpers/networkModelODE.R
+++ helpers/r_helpers/networkModelODE.R
@@ -2,6 +2,21 @@ require(deSolve)
 
 dx_dt = function(t, state, params){
   
+  num_nodes = length(state)
+  
+  dxdt = rep(NA, num_nodes)
+  
+  s = params['s']
+  g = params['g']
+  tau = params['tau']
+  sigma = params['sigma']
+  
+  for(i in 1:num_nodes){
+    spont_act = rnorm(1, mean=0, sd= sigma)
+    dxdt[i] = (-state[i] + s*phi(state[i]) + g * ... + spont_act)/tau 
+  }
+  
+  return(list(dxdt))
 }
 
 
