44b62b09b4c65433cba6682a41bbc2be4afc5a36
Zeynep Enkavi <zeynepenkavi@gmail.com>
2021-05-07 13:19:58 -0700

    done w first draft of multreg ci fun

--- helpers/get_multreg_fc_wci.py
+++ helpers/get_multreg_fc_wci.py
@@ -1,7 +1,27 @@
 import numpy as np
-from scipy import stats
+import statsmodels.api as sm
 
 def get_multreg_fc_wci(activity_matrix, network):
+    
+    nnodes = activity_matrix.shape[0]
+    
+    multreg_fc = np.zeros((nnodes,nnodes))
+    multreg_fc_lb = np.zeros((nnodes,nnodes))
+    multreg_fc_ub = np.zeros((nnodes,nnodes))
+    
+    for targetnode in tange(nnodes):
+        othernodes = list(range(nnodes))
+        othernodes.remove(targetnode) # Remove target node from 'other nodes'
+        X = activity_matrix[othernodes,:].T
+        X = sm.add_constant(X)
+        y = activity_matrix[targetnode,:]
+        mod = sm.OLS(y, X)
+        res = mod.fit()
+        
+        # Select starting from 1 to exclude intercept
+        multreg_fc[targetnode,othernodes]=res.params[1:]
+        multreg_fc_lb[targetnode,othernodes] = [x[0] for x in res.conf_int()[1:]]
+        multreg_fc_ub[targetnode,othernodes] = [x[1] for x in res.conf_int()[1:]]
     
     # Compute RMSE
     multreg_fc_rmse = np.sqrt(np.mean(np.square(multreg_fc - network['W'])))
